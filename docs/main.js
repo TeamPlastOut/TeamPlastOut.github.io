3cbf0d277647d52bc68331740a82ebad5163cb50
TeamPlastOut <teamplastout@gmail.com>
2025-03-23 17:14:10 -0700

    navbar

--- docs/main.js
+++ docs/main.js
@@ -1,2 +1,21 @@
-function openNav() {
-}
+// const menu_button = document.getElementById("menu-button");
+// const nav = document.getElementById("navigator");
+
+// function openNav() {
+//     menu_button.onclick = closeNav
+
+//     nav.style.height = "100%";
+//     nav.style.width = "100%";
+
+//     nav.style.opacity = 0.75;
+//     nav.style.backgroundColor = "#474747";
+
+//     nav.onclick = closeNav;
+// }
+
+// function closeNav() {
+//     menu_button.onclick = openNav;
+
+//     nav.style.height = "0px";
+//     nav.style.width = "0px";    
+// }
